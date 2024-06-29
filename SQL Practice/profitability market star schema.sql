afbc97a2ba66c8923cc290381a4e82e56cd40b66
Himanshu Bhatt <66287451+desininja@users.noreply.github.com>
2024-06-29 14:05:15 +0530

    Finished SQL Practice

--- SQL Practice/profitability market star schema.sql
+++ SQL Practice/profitability market star schema.sql
@@ -63,19 +63,96 @@ order by 2 DESC;
 
 -- Average profit per order
 
-select 
 
 
 -- Average profit percentage per order
 
+/*
+Problem statement: Extract the details of the top ten customers in the expected output format.
+
+Expected Output form: cust_id, rank, customer_name, profit, customer_city, customer_state, sales.
+Tables: The tables that are required for solving this problem are as follows:
+
+'cust_dimen'
+'market_fact_full'
+
+*/
+ use market_star_schema;
+select * from cust_dimen limit 4;
+select * from market_fact_full limit 4;
+
+select  cust_id, 
+		rank_ as 'rank', 
+        customer_name,
+        total_profit as profit, 
+        city as customer_city, 
+        state as customer_state, 
+        round(sales,2) as sales
+from (
+select cd.*,
+		sum(profit) as total_profit,
+        sum(sales) as sales,
+        rank() over(order by sum(profit) DESC ) as rank_,
+        dense_rank() over(order by sum(profit) DESC ) as rank_dense
+        from cust_dimen as cd
+        inner join market_fact_full as mff on mff.cust_id= cd.cust_id
+        group by cust_id ) as temp
+        where rank_ <=10 and rank_dense <=10;
+        
 
 
+/*
+Problem statement: Extract the required details of the customers who have not placed an order yet.
+Expected columns: The columns that are required as the output are as follows:
+
+'cust_id'
+'cust_name'
+'city'
+'state'
+'customer_segment'
+A flag to indicate that there is another customer with the exact same name and city but a different customer ID.
+Tables: The tables that are required for solving this problem are as follows:
+
+'cust_dimen'
+'market_fact_full'
+*/
 
 
+select cdd.*
+ from cust_dimen cdd
+where cdd.cust_id IN (
+select distinct cd.cust_id
+from cust_dimen as cd 
+left outer join market_fact_full as mff 
+on mff.cust_id = cd.cust_id 
+where mff.ord_id is null)
+order by cdd.customer_name;
 
+-- No such customer with no order palced.
 
+-- Finding customers that have placed multiple order.
+with orders_cte as (
+select cd.*,
+		count(ord_id) as number_of_orders
+from cust_dimen as cd
+left join market_fact_full as mff
+on mff.cust_id = cd.cust_id
+group by  cust_id
+having count(ord_id) >1
+),
 
+fraud_check_cte as (
+select customer_name,
+		city,
+        count(cust_id) as customer_id_count
+from cust_dimen
+group by customer_name, city
+)
 
-
-
-
+select oc.* ,
+		case 
+			when fcc.customer_id_count >1 then 'FRAUD' else 'NORMAL' END as FRAUD_TAG
+from orders_cte as oc 
+left join fraud_check_cte as fcc 
+on fcc.customer_name = oc.customer_name 
+and fcc.city = oc.city
